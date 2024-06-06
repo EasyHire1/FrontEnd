d73803fcb1719f996c32e0cc9d09470097b45a01
sundos gutty <sundosgutty@gmail.com>
2024-06-06 22:46:05 +0300

    update typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
 
 function App() {
 
   return (
     <Router>
-     <App />
+      <div>Hello</div>
     </Router>
   )
 }
